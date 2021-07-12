5f542f78e3e89f8c754d7cad1ed377f453b5cff9
Sitan Cai <sitanc@uci.edu>
2021-07-12 23:59:24 +0800

    Farm assets test done

--- Assets/Scripts/FarmDetector.cs
+++ Assets/Scripts/FarmDetector.cs
@@ -9,7 +9,8 @@ public class FarmDetector : MonoBehaviour
     public GameObject seed;
 
 
-    private bool flag = true;
+    private bool plant = true;
+    private bool flag = false;
     void Start()
     {
         
@@ -19,20 +20,38 @@ public class FarmDetector : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        flag = false;
+        plant = false;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        flag = true;
+        plant = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "soil")
+        {
+            flag = true;
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "soil")
+        {
+            flag = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        flag = false;
     }
     void Update()
     {
         
-        if (flag && Input.GetButtonDown("Fire1"))
+        if (flag && plant && Input.GetButtonDown("Fire1"))
         {
 
             GameObject newSeed = Instantiate(seed);
-            newSeed.transform.position = new Vector3 (gameObject.transform.position.x, (float)(gameObject.transform.position.y - 0.5));
+            newSeed.transform.position = new Vector3 (gameObject.transform.position.x, (float)(gameObject.transform.position.y - 0.45));
             newSeed.SetActive(true);
             
         }

2dca5bde2a0838d4cb9315f4f89ed7c406cb1db4
jingyuc4 <60535811+jingyuc4@users.noreply.github.com>
2021-07-13 10:18:19 -0700

    fish_game

---
+++ Assets/eat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class eat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+        Destroy(other.gameObject);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
